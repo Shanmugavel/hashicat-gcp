04363df6057ae43431f503703ca95084292ff223
Shanmugavel <shanmugavel.rs@gmail.com>
2021-11-05 13:18:08 +0000

    fix

--- vpc.tf
+++ vpc.tf
@@ -8,21 +8,20 @@ module "vpc" {
 
   subnets = [
     {
-      subnet_name   = "gaurav-subnet"
+      subnet_name   = "shan-subnet"
       subnet_ip     = "10.100.10.0/24"
       subnet_region = var.region
     }
   ]
 
   secondary_ranges = {
-    subnet-01 = [
+    shan-subnet = [
       {
-        range_name    = "subnet-01-secondary-01"
+        range_name    = "shan-subnet-secondary-01"
         ip_cidr_range = "192.168.64.0/24"
       },
     ]
 
-    subnet-02 = []
   }
 
   routes = [
