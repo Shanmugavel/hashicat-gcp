deeae948a25553065ffe3402641d818ae021ca89
Shanmugavel <shanmugavel.rs@gmail.com>
2021-11-05 13:28:37 +0000

    Fixed

--- vpc.tf
+++ vpc.tf
@@ -1,36 +1,14 @@
-module "vpc" {
-  source  = "terraform-google-modules/network/google"
-  version = "~> 3.0"
-
-  project_id   = var.project
+module "network" {
+  source  = "app.terraform.io/ricardo-gcp-training/network/google"
+  version = "3.4.0"
+  # insert required variables here
   network_name = "shan-network"
-  routing_mode = "GLOBAL"
-
+  project_id   = var.project
   subnets = [
     {
       subnet_name   = "shan-subnet"
       subnet_ip     = "10.100.10.0/24"
       subnet_region = var.region
-    }
-  ]
-
-  secondary_ranges = {
-    shan-subnet = [
-      {
-        range_name    = "shan-subnet-secondary-01"
-        ip_cidr_range = "192.168.64.0/24"
-      },
-    ]
- 
-  }
-
-  routes = [
-    {
-      name              = "egress-internet"
-      description       = "route through IGW to access internet"
-      destination_range = "0.0.0.0/0"
-      tags              = "egress-inet"
-      next_hop_internet = "true"
     }
   ]
 }
